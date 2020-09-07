143db05e89f9a6b975b4ebc265365cb0c74995e6
child_duckling <19170969+child-duckling@users.noreply.github.com>
2020-09-07 13:52:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   var height = 300;
 
   chrome.app.window.create('index.html', {
-    id: "helloWorldID",
+    id: "Chrome OS Updates",
     outerBounds: {
       width: width,
       height: height,
